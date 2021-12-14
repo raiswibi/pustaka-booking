5900ee1940db101590222fb820b44ae298262f34
raiswibi <mohamadrais1911@agmail.com>
2021-12-15 00:13:40 +0700

    menambahkan function penjumlahan

--- Model_latihan1.php
+++ Model_latihan1.php
@@ -2,7 +2,7 @@
 {
     //membuat variable untuk menampung nilai     public $nilai1, $nilai2, $hasil; 
 
-    public function jumlah($n1 = null, $n2 = null)
+    public function jumlah($nil1 = null, $nil2 = null)
     {
         $this->nilai1 = $nil1;
         $this->nilai2 = $nil2;
